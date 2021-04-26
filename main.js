6156f1170ec5303d28b98d2a68f4e61d4a15a364
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-04-26 19:17:44 +0530

    done

--- main.js
+++ main.js
@@ -16,15 +16,35 @@ const randomFunc = {
     sysmbols:getRandomSymbol
 }
 
+// Generate Event
 generateEl.addEventListener('click',()=>{
 
     const length = +lengthEl.value;
     const hasLower = lowercaseEl.checked;
-    const has
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
 
-    console.log(typeof length)
+    resultsEl.innerText = generatePassword(hasLower,hasUpper,hasNumber,hasSymbol);
+});
 
-})
+// Generate Password function
+function generatePassword(lower,upper,number,symbols,length){
+    //  1. Init pw var
+    //  2. Filter out unchecked types
+    //  3. Loop over length call generator function for each type
+    //  4. Add final pw to the pw var and return  
+
+    let generatedPassword = '';
+
+    const typeCount = lower + upper + number + symbols;
+
+    console.log('typeCount:', typeCount);
+
+    const typesArr = [{lower},{upper},{number},{symbols}];
+
+    console.log(typesArr);
+}
 
 // Generator Function
 function getRandomLower(){
