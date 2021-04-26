030c10d2fa99c101ef071fc25182314bbb4764a5
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-04-26 19:18:00 +0530

    done

--- main.js
+++ main.js
@@ -1,68 +1,86 @@
-// DOM Elements
-const resultsEl = document.getElementById('result');
+const resultEl = document.getElementById('result');
 const lengthEl = document.getElementById('length');
 const uppercaseEl = document.getElementById('uppercase');
 const lowercaseEl = document.getElementById('lowercase');
 const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
-const clipboardEl = document.getElementById('clipboard');
-
+const clipboard = document.getElementById('clipboard');
 
 const randomFunc = {
-    lower:getRandomLower,
-    uppercase:getRandomUpper,
-    number:getRandomNumber,
-    sysmbols:getRandomSymbol
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol
 }
 
-// Generate Event
-generateEl.addEventListener('click',()=>{
-
-    const length = +lengthEl.value;
-    const hasLower = lowercaseEl.checked;
-    const hasUpper = uppercaseEl.checked;
-    const hasNumber = numbersEl.checked;
-    const hasSymbol = symbolsEl.checked;
-
-    resultsEl.innerText = generatePassword(hasLower,hasUpper,hasNumber,hasSymbol);
+clipboard.addEventListener('click', () => {
+	const textarea = document.createElement('textarea');
+	const password = resultEl.innerText;
+	
+	if(!password) { return; }
+	
+	textarea.value = password;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	textarea.remove();
+	alert('Password copied to clipboard');
 });
 
-// Generate Password function
-function generatePassword(lower,upper,number,symbols,length){
-    //  1. Init pw var
-    //  2. Filter out unchecked types
-    //  3. Loop over length call generator function for each type
-    //  4. Add final pw to the pw var and return  
+generate.addEventListener('click', () => {
+	const length = +lengthEl.value;
+	const hasLower = lowercaseEl.checked;
+	const hasUpper = uppercaseEl.checked;
+	const hasNumber = numbersEl.checked;
+	const hasSymbol = symbolsEl.checked;
+	
+	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
 
-    let generatedPassword = '';
-
-    const typeCount = lower + upper + number + symbols;
-
-    console.log('typeCount:', typeCount);
-
-    const typesArr = [{lower},{upper},{number},{symbols}];
-
-    console.log(typesArr);
+function generatePassword(lower, upper, number, symbol, length) {
+	let generatedPassword = '';
+	const typesCount = lower + upper + number + symbol;
+	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+	
+	// Doesn't have a selected type
+	if(typesCount === 0) {
+		return '';
+	}
+	
+	// create a loop
+	for(let i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+	
+	const finalPassword = generatedPassword.slice(0, length);
+	
+	return finalPassword;
 }
 
-// Generator Function
-function getRandomLower(){
-    return String.fromCharCode(Math.floor(Math.random() * 26) +97);
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-function getRandomUpper(){
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-function getRandomNumber(){
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
-function getRandomSymbol(){
-    const sysmbols = '!@#$%^&*()_+';
-    return sysmbols[Math.floor(Math.random() * sysmbols.length)];
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 
-console.log(getRandomUpper());
+
+
+
+
+
